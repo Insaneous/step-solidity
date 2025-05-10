c5dcf83ced40ddac15132f5931415df5d6ee6621
Insaneous <insaneous@proton.me>
2025-05-10 20:20:38 +0500

    exam

--- exam/white.sol
+++ exam/white.sol
@@ -3,22 +3,28 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721URIStorage, ERC2981 {
+contract Token is ERC721URIStorage, ERC2981, Ownable {
     uint totalSupply;
     bool whiteListToggle = true;
     mapping(address => bool) whiteList;
-    constructor() ERC721URIStorage("Name", "NNN") {
+    constructor() ERC721URIStorage("Token", "TTT") Ownable(msg.sender) {
         _setDefaultRoyalty(msg.sender, 100);
     }
 
     function mint(uint _amount) external payable {
         if (whiteListToggle) {
-            require();
+            require(whiteList[msg.sender]);
         }
         for (uint i = 0; i < _amount; i++){
-           _safeMint();
+            uint tokenId = totalSupply++;
+           _safeMint(msg.sender, tokenId);
         }
+    }
+
+    function toggleWhiteList() external onlyOwner {
+        whiteListToggle = !whiteListToggle;
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override (ERC721URIStorage, ERC2981) returns (bool) {
